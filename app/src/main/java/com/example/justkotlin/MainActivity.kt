08ff9bd7d5465f241c5bb2b3d152216a7f405e6d
olisheikh <olisheikh707@gmail.com>
2022-08-19 15:26:52 +0600

    Final Commit

--- app/src/main/java/com/example/justkotlin/MainActivity.kt
+++ app/src/main/java/com/example/justkotlin/MainActivity.kt
@@ -8,17 +8,36 @@ import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(){
+    // Global variables
+    var numberOfCoffee = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
     }
-    var n = 0
+
+    fun incrementBtn(view: View){
+        numberOfCoffee++
+        display(numberOfCoffee)
+    }
+
+    fun decrementBtn(view: View){
+        numberOfCoffee--
+        display(numberOfCoffee)
+    }
+
+    // Display text on TextView
+    fun display(numberOfCoffeePar: Int){
+        var txtDisplay = findViewById<TextView>(R.id.numberOfCoffeeId)
+
+        txtDisplay.text = "$numberOfCoffeePar"
+    }
 
     fun submitOrder(view: View) {
-        var txtView = findViewById<TextView>(R.id.showNumbers)
-        n++;
-        txtView.text = "$n"
+        var totalTxtView = findViewById<TextView>(R.id.totalPriceId)
+
+        "Total Price:$${numberOfCoffee * 5}\nThank your!".also { totalTxtView.text = it }
     }
 
 }
